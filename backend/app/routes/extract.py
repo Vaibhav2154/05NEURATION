2b1abe63084b752482a2ab29626030da9b8464e0
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-04-11 17:26:37 +0530

    remove unwanted

--- backend/app/routes/extract.py
+++ backend/app/routes/extract.py
@@ -1,11 +1,8 @@
 from flask import Blueprint, request, jsonify
 from app.services.genai import extract_fields
 
-<<<<<<< HEAD
 extract_bp = Blueprint('extract', __name__)
-=======
-extract_bp = Blueprint('extract_bp', __name__)  # ✅ name must match in registration!
->>>>>>> 3fb081452ea1b347b8cbba976d5e7395aa7f2d82
+extract_bp = Blueprint('extract_bp', __name__)  # ✅
 
 @extract_bp.route('/extract', methods=['POST'])
 def extract():
